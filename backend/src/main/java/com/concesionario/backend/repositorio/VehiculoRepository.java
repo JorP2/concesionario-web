8cd5e9ff450c9c919917fb4edbeef9541bfbef73
TunnelSnxke <alexxy2j@gmail.com>
2026-03-06 12:19:36 +0100

    Repository vehiculo - Alejandro

--- backend/src/main/java/com/concesionario/backend/repositorio/VehiculoRepository.java
+++ backend/src/main/java/com/concesionario/backend/repositorio/VehiculoRepository.java
@@ -1,9 +1,11 @@
 package com.concesionario.backend.repositorio;
 
-// paquetes jpa (CRUD)
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.concesionario.backend.dominio.Vehiculo;
 
 public interface VehiculoRepository extends JpaRepository<Vehiculo, Long>{
-
+	
+	List<Vehiculo>findByMarca(String marca);
+	List<Vehiculo>findByCombustible(String combustible);
 }
