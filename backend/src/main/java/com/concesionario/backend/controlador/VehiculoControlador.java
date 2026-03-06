243b33d7339312fba2d4dc88608b9eed2c27dc9b
Jordy <panorajordy25@gmail.com>
2026-03-06 12:32:45 +0100

    Cambios de estructura frontend

--- backend/src/main/java/com/concesionario/backend/controlador/VehiculoControlador.java
+++ backend/src/main/java/com/concesionario/backend/controlador/VehiculoControlador.java
@@ -1,27 +1,33 @@
 package com.concesionario.backend.controlador;
 
 import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import com.concesionario.backend.dominio.Vehiculo;
 import com.concesionario.backend.repositorio.VehiculoRepository;
 
+// Marca la clase como API que devuelve JSON
 @RestController
-@RequestMapping("/vehiculos")
+// Define la ruta base para los endpoints de este controlador
+@RequestMapping("/api/vehiculos")
+// Permite que el frontend consuma la API sin errores CORS
+@CrossOrigin(origins = "*")
 public class VehiculoControlador {
-	
-	@Autowired
-    private VehiculoRepository repo;
 
-    // 🔹 LISTAR TODOS
+    private final VehiculoRepository repo;
+
+    // Constructor para inyectar automáticamente el repositorio
+    public VehiculoControlador(VehiculoRepository repo) {
+        this.repo = repo;
+    }
+
+    // Endpoint - GET obtener vehiculos
     @GetMapping
     public List<Vehiculo> listar() {
         return repo.findAll();
     }
 
-    // 🔹 GUARDAR
+ // Endpoint - POST guardar vehiculo
     @PostMapping
     public Vehiculo guardar(@RequestBody Vehiculo vehiculo) {
         return repo.save(vehiculo);
--- backend/src/main/java/com/concesionario/backend/repositorio/VehiculoRepository.java
+++ backend/src/main/java/com/concesionario/backend/repositorio/VehiculoRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.concesionario.backend.dominio.Vehiculo;
 
 public interface VehiculoRepository extends JpaRepository<Vehiculo, Long>{
-
+	
+	// Filtros
 }
