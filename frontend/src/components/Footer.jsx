3f1bbb1deba1faf21581f4c1cf7a6125b33d996d
TunnelSnxke <alexxy2j@gmail.com>
2026-03-10 12:47:29 +0100

    Actualización footer .jsx y .css - Alejandro

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,24 +1,28 @@
 import "../styles/Footer.css";
-import { MdEmail, MdPhone } from "react-icons/md";
+import { MdEmail, MdPhone, MdLocationOn, MdHome, MdAccessTime } from "react-icons/md";
 
 function Footer() {
   return (
     <footer>
-      <div>
-        <h3>Nohales Automoviles</h3>
-        <p>Vehículos de confianza</p>
+      <div className="footer-logo-div">
+        <img src="/logoNohalesAutomoviles.png" alt="Logo Nohales Automoviles" className="footer-logo" />
       </div>
 
-      <div>
-        <h3>Horario:</h3>
+      <div className="footer-div">
+        <h3>Horarios:</h3>
         <ul>
-          <li>Lunes a viernes: 09:30 - 13:30 | 16:30 - 20:00</li>
-          <li>Sabados: 10:00 - 14:00</li>
-          <li></li>
+          <li><MdAccessTime/> Lunes a viernes: 09:30 - 13:30 | 16:30- 20:00</li>
+          <li><MdAccessTime/> Sabados: 10:00 - 14:00</li>
         </ul>
       </div>
 
-      <div>
+      <div className="footer-div">
+        <h3>Dirección:</h3>
+        <p><MdLocationOn/> 45112 Burguillos de Toledo (Toledo)</p>
+        <p><MdHome/> C/ Caño S/N</p>
+      </div>
+
+      <div className="footer-div">
         <h3>Contáctanos</h3>
         <p><MdEmail/> nohalesautomoviles@gmail.com</p>
         <p><MdPhone/> +34 651 86 82 30</p>
