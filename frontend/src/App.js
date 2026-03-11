b716a4ca6ac1f5b9143b23a25774efe1fd7467ec
Jordy <panorajordy25@gmail.com>
2026-03-11 11:46:14 +0100

    NavBar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
+import { Navbar } from "./components/Navbar";
 import Vehiculos from "./pages/Vehiculos";
 
 function App() {
   return (
     <div>
+      <Navbar />
       <h1>Concesionario</h1>
       <Vehiculos />
     </div>
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,105 @@
+import React from 'react'
+
+// Barra Superior de Navegación
+export const Navbar = () => {
+  return (
+    <>
+    {/* Titulo posible logo */}
+        {/* Navbar concesionario */}
+    <nav className='navbar navbar-expand-lg navbar-dark bg-dark shadow'>
+      <div className='container-fluid'>
+
+        {/* Logo / Nombre */}
+        <a className='navbar-brand fw-bold' href='/'>
+          Concesionario
+        </a>
+
+        {/* Botón hamburguesa */}
+        <button
+          className='navbar-toggler'
+          type='button'
+          data-bs-toggle='collapse'
+          data-bs-target='#navbarMain'
+          aria-controls='navbarMain'
+          aria-expanded='false'
+          aria-label='Toggle navigation'
+        >
+          <span className='navbar-toggler-icon'></span>
+        </button>
+
+        {/* Links */}
+        <div className='collapse navbar-collapse' id='navbarMain'>
+          <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
+
+            {/* Coches */}
+            <li className='nav-item'>
+              <a className='nav-link active' href='/vehiculos'>
+                Coches
+              </a>
+            </li>
+
+            {/* Nosotros */}
+            <li className='nav-item'>
+              <a className='nav-link' href='/nosotros'>
+                Nosotros
+              </a>
+            </li>
+
+            {/* Contacto dropdown */}
+            <li className='nav-item dropdown'>
+              <a
+                className='nav-link dropdown-toggle'
+                href='/contacto'
+                role='button'
+                data-bs-toggle='dropdown'
+                aria-expanded='false'
+              >
+                Contacto
+              </a>
+
+              <ul className='dropdown-menu'>
+                <li>
+                  <a className='dropdown-item' href='/'>
+                    WhatsApp
+                  </a>
+                </li>
+
+                <li>
+                  <a className='dropdown-item' href='/'>
+                    Gmail
+                  </a>
+                </li>
+
+                <li><hr className='dropdown-divider' /></li>
+
+                <li>
+                  <a className='dropdown-item' href='/contacto'>
+                    Formulario
+                  </a>
+                </li>
+              </ul>
+            </li>
+
+          </ul>
+
+          {/* Buscador */}
+          <form className='d-flex' role='search'>
+            <input
+              className='form-control me-2'
+              type='search'
+              placeholder='Buscar coche...'
+              aria-label='Buscar'
+            />
+
+            <button className='btn btn-outline-light' type='submit'>
+              Buscar
+            </button>
+          </form>
+
+        </div>
+      </div>
+    </nav>
+    </>
+  )
+}
+
