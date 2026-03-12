d83dbc003201994f85854ed1ad08bfac5b53b958
TunnelSnxke <alexxy2j@gmail.com>
2026-03-12 10:26:42 +0100

    Cambio a bootstrap del footer - alejandro

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,31 +1,41 @@
-import "../styles/Footer.css";
 import { MdEmail, MdPhone, MdLocationOn, MdHome, MdAccessTime } from "react-icons/md";
 
 function Footer() {
   return (
-    <footer>
-      <div className="footer-logo-div">
-        <img src="/logoNohalesAutomoviles.png" alt="Logo Nohales Automoviles" className="footer-logo" />
-      </div>
+    <footer className="bg-dark text-white py-4 mt-5">
+      <div className="container">
+        <div className="row text-center text-md-start align-items-start">
 
-      <div className="footer-div">
-        <h3>Horarios:</h3>
-        <ul>
-          <li><MdAccessTime/> Lunes a viernes: 09:30 - 13:30 | 16:30- 20:00</li>
-          <li><MdAccessTime/> Sábados: 10:00 - 14:00</li>
-        </ul>
-      </div>
+          <div className="col-12 col-md-3 mb-4 mb-md-0 text-center">
+            <img 
+              src="/logoNohalesAutomoviles.png"
+              alt="Logo Nohales Automoviles"
+              className="img-fluid"
+              style={{ maxWidth: "180px" }}
+            />
+          </div>
 
-      <div className="footer-div">
-        <h3>Dirección:</h3>
-        <p><MdLocationOn/> 45112 Burguillos de Toledo (Toledo)</p>
-        <p><MdHome/> C/ Caño S/N</p>
-      </div>
+          <div className="col-12 col-md-3 mb-4 mb-md-0">
+            <h5>Horarios</h5>
+            <ul className="list-unstyled">
+              <li><MdAccessTime className="me-2"/> Lunes a viernes: 09:30 - 13:30 | 16:30 - 20:00</li>
+              <li><MdAccessTime className="me-2"/> Sábados: 10:00 - 14:00</li>
+            </ul>
+          </div>
 
-      <div className="footer-div">
-        <h3>Contáctanos</h3>
-        <p><MdEmail/> nohalesautomoviles@gmail.com</p>
-        <p><MdPhone/> +34 651 86 82 30</p>
+          <div className="col-12 col-md-3 mb-4 mb-md-0">
+            <h5>Dirección</h5>
+            <p><MdLocationOn className="me-2"/> 45112 Burguillos de Toledo (Toledo)</p>
+            <p><MdHome className="me-2"/> C/ Caño S/N</p>
+          </div>
+
+          <div className="col-12 col-md-3">
+            <h5>Contáctanos</h5>
+            <p><MdEmail className="me-2"/> nohalesautomoviles@gmail.com</p>
+            <p><MdPhone className="me-2"/> +34 651 86 82 30</p>
+          </div>
+
+        </div>
       </div>
     </footer>
   );
