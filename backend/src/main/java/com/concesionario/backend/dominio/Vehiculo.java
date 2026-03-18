1ce3a17b8069119b80d02666fed6d538df8a2480
TunnelSnxke <alexxy2j@gmail.com>
2026-03-18 12:05:35 +0100

    Resolucion

--- backend/src/main/java/com/concesionario/backend/dominio/Vehiculo.java
+++ backend/src/main/java/com/concesionario/backend/dominio/Vehiculo.java
@@ -48,7 +48,7 @@ public class Vehiculo {
 	@NotBlank(message = "Cambio no puede quedar vacio")
 	@Column(nullable = false)
 	private String marchas;
-	@NotNull(message = "Descripcion no puede quedar vacio")
+	@NotBlank(message = "Descripcion no puede quedar vacio")
 	@Column(nullable = false)
 	private String descripcion;
 	private String extras;
@@ -79,13 +79,8 @@ public class Vehiculo {
 	public void setPuertas(Integer puertas) { this.puertas = puertas; }
 	public String getMotor() { return motor; }
 	public void setMotor(String motor) { this.motor = motor; }
-	public String getMarchas() {
-		return marchas;
-	}
-
-	public void setMarchas(String marchas) {
-		this.marchas = marchas;
-	}
+	public String getMarchas() { return marchas; }
+	public void setMarchas(String marchas) { this.marchas = marchas; }
 	public String getDescripcion() { return descripcion; }
 	public void setDescripcion(String descripcion) { this.descripcion = descripcion; }
 	public String getExtras() { return extras; }
